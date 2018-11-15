f7ea2b82b3ab0b34be2ab41e1d26fcf2af2cea6e
Basuki Winoto <baw942@g.harvard.edu>
2018-11-15 11:54:12 -0500

    sample for PCF

---
+++ src/main/groovy/io/cfapps/basukiwinoto/BasukiwinotoApplication.groovy
@@ -0,0 +1,12 @@
+package io.cfapps.basukiwinoto
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BasukiwinotoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run BasukiwinotoApplication, args
+	}
+}
---
+++ src/test/groovy/io/cfapps/basukiwinoto/BasukiwinotoApplicationTests.groovy
@@ -0,0 +1,16 @@
+package io.cfapps.basukiwinoto
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class BasukiwinotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
